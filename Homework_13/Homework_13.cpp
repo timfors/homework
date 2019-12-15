9350084ed4a8acff2e978258b7feca799234852e
= <=>
2019-12-16 00:08:27 +0300

    update homework

---
+++ Homework_13/Homework_13.cpp
@@ -0,0 +1,58 @@
+﻿// Homework_13.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	string str, res = "";;
+	char ind;
+
+	cout << "enter index" << endl;
+	cin >> ind;
+	cout << "enter string" << endl;
+	cin >> str;
+	
+
+	char actualSymb = str[1], prevSymb = str[0];
+	if (ind == 'n') {
+		int counter = 1;
+		for (int i = 1; i < str.length(); i++) {
+			actualSymb = str[i];
+			if (actualSymb == prevSymb)
+				counter++;
+			else {
+				res += prevSymb;
+				if (counter > 1)
+					res += (counter + '0');
+				counter = 1;
+				prevSymb = actualSymb;
+			}
+		}
+		if (counter > 1) {
+			res += actualSymb;
+			if (counter > 1)
+				res += (counter + '0');
+			counter = 1;
+		}
+		else {
+			res += actualSymb;
+		}
+	}
+	else if (ind == 'm') {
+		for (int i = 1; i < str.length(); i++) {
+			actualSymb = str[i];
+			if (actualSymb > '1' && actualSymb <= '9')
+				for (int j = 0; j < actualSymb - '0'; j++)
+					res += prevSymb; 
+			else {
+				res += actualSymb;
+				prevSymb = actualSymb;
+			}
+		}
+	}
+
+	cout << res;
+}
+
---
+++ Homework_9/Homework_9.cpp
@@ -0,0 +1,31 @@
+﻿
+#include <iostream>
+#include <map>
+using namespace std;
+
+int main()
+{
+	int n, t, result = 0;
+	cout << "enter N" << endl;
+	cin >> n;
+	cout << "enter array" << endl;
+	int* arr = new int[n];
+	for (int i = 0; i < n; i++)
+		cin >>arr[i];
+
+	cout << "enter T" << endl;
+	cin >> t;
+
+	map <int, bool> foundedNumbers;
+
+
+	for (int i = 0; i < n; i++) {
+		int num = arr[i];
+		if (foundedNumbers[t - num]) 
+			result++;
+		else 
+			foundedNumbers[num] = true;
+	}
+
+	cout << result;
+}
